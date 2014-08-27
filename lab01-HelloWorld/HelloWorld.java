bd36e3a7a25d0cdceb48a3ff9a00750e584553ea
Alana Corey <abcorey@optonline.net>
2014-08-27 15:27:55 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+//Alana Corey
+// HW1
+// Hellow World Java Program
+
+public class HelloWorld{
+  public static void main(String[ ] args) {
+    
+    System.out.println("#######################");
+    System.out.println("#######################");
+    System.out.println("#                     #");
+    System.out.println("#  Hello World 2014!  #");
+    System.out.println("#                     #");
+    System.out.println("#######################");
+    System.out.println("#######################");
+    System.out.println("test");
+}
+}
