2deb7b9672e1ea500fa1843274bafc54a4052572
Alana Corey <abcorey@optonline.net>
2014-08-31 18:53:05 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,19 @@
+// hw01
+// Alana Corey
+
+public class WelcomeClass{
+
+  public static void main(String[ ] args) {
+    
+    System.out.println("-----------");
+    System.out.println("| Welcome |");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-a--b--c--2--1--6-|" );
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("I'm traveling abroad to Japan next semester and my main goal is to hike to the top of mt. fuji");
+    
+    }
+}
